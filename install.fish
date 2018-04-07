5dbdbc2892190d2dfd9befd47aa8a065dd1fe506
Rick Henry <fredericmhenry@gmail.com>
2018-04-07 13:32:45 -0400

    try adding completions
    Signed-off-by: Rick Henry <fredericmhenry@gmail.com>

--- install.fish
+++ install.fish
@@ -1,4 +1,6 @@
+echo "running install script"
 if not test -f /usr/share/fish/completions/snap.fish
+  echo "downloading file"
   mkdir $path/completions
   curl -L https://raw.githubusercontent.com/fish-shell/fish-shell/master/share/completions/snap.fish > $path/completions/snap.fish
 end
