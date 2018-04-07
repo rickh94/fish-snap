939b6119e7a661d6defaddb0268c4eaac92ac9ac
Rick Henry <fredericmhenry@gmail.com>
2018-04-07 15:36:51 -0400

    maybe bass will save us
    Signed-off-by: Rick Henry <fredericmhenry@gmail.com>

--- init.fish
+++ init.fish
@@ -4,11 +4,12 @@ set -l snappath (grep --color=never "export PATH" $snapscript | awk -F":" '{prin
 set -l snapdesktop (grep --color=never "export XDG" $snapscript | awk -F":" '{print $NF}' |sed 's/\"//')
 if test -d $snappath;
 and test -d $snapdesktop
-  contains -- $snappath $PATH
-  or set -gx PATH $PATH $snappath
-  if not echo $XDG_DATA_DIRS |grep -q $snapdesktop
-    set -gx XDG_DATA_DIRS $XDG_DATA_DIRS:$snapdesktop
-  end
+  bass $snapscript
+  #contains -- $snappath $PATH
+  #or set -gx PATH $PATH $snappath
+  #if not echo $XDG_DATA_DIRS |grep -q $snapdesktop
+  #  set -gx XDG_DATA_DIRS $XDG_DATA_DIRS:$snapdesktop
+  #end
 else
   echo "Cannot find snap bin or desktop files"
 end
