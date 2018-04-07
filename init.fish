18240f23c0bc8066409f08d61503935d019378f6
Rick Henry <fredericmhenry@gmail.com>
2018-04-07 15:39:05 -0400

    ugh
    Signed-off-by: Rick Henry <fredericmhenry@gmail.com>

--- init.fish
+++ init.fish
@@ -4,7 +4,7 @@ set -l snappath (grep --color=never "export PATH" $snapscript | awk -F":" '{prin
 set -l snapdesktop (grep --color=never "export XDG" $snapscript | awk -F":" '{print $NF}' |sed 's/\"//')
 if test -d $snappath;
 and test -d $snapdesktop
-  bass $snapscript
+  bass 'source $snapscript'
   #contains -- $snappath $PATH
   #or set -gx PATH $PATH $snappath
   #if not echo $XDG_DATA_DIRS |grep -q $snapdesktop
