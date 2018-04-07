8400dbff790ce8bd97526d870ed65e64fe57d000
Rick Henry <fredericmhenry@gmail.com>
2018-04-07 15:06:51 -0400

    ugh
    Signed-off-by: Rick Henry <fredericmhenry@gmail.com>

--- init.fish
+++ init.fish
@@ -6,8 +6,9 @@ if test -d $snappath;
 and test -d $snapdesktop
   contains -- $snappath $PATH
   or set -gx PATH $PATH $snappath
-  echo $XDG_DATA_DIRS |grep -q $snapdesktop
-  or set -gx XDG_DATA_DIRS /usr/share:/usr/local/share:$snapdesktop
+  if not echo $XDG_DATA_DIRS |grep -q $snapdesktop
+    set -gx XDG_DATA_DIRS /usr/share:/usr/local/share:$snapdesktop
+  end
 else
   echo "Cannot find snap bin or desktop files"
 end
